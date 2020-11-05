fd14b46539e5918b20792ae3b5c12a120d0f6287
jbahm <bahm@chapman.edu>
2020-11-05 12:34:04 -0800

    Updated schema

--- GameDB.sql
+++ GameDB.sql
@@ -1,51 +1,71 @@
-Genre(genreID, genre)
-Studio(studioID, name, yearFounded)
+Genre(genreID, name)
+Studios(studioID, name, yearFounded, numEmployees)
 Developer(devID, name, yearsActive, employer)
-Franchise(franchiseID, name)
-WorldRecords(recordID, game, console, playerName, bestTime)
-Console(consoleID, name, sales, releaseYear, type);
-Game(gameID, title, console, releaseDate, sales, genre, franchise, studio, leadDev, averageTime, worldRecord)
+Franchises(franchiseID, name)
+Records(recordID, statsID, title, player, bestTime)
+Stats(statsID, gameID, avgTimeToBeat)
+Console(consoleID, name, sales, releaseYear, type)
+Games(gameID, title, console, releaseDate, sales, genre, franchise, studio, leadDev, averageTime, worldRecord)
 
 CREATE TABLE IF NOT EXISTS Genre(
   genreID integer NOT NULL PRIMARY KEY,
-  genre varchar(50)
+  name varchar(50)
 );
 
-CREATE TABLE IF NOT EXISTS Studio(
+CREATE TABLE IF NOT EXISTS Studios(
   studioID integer NOT NULL PRIMARY KEY,
   name varchar(50),
-  yearFounded integer
+  yearFounded integer,
+  numEmployees integer
 );
 
 CREATE TABLE IF NOT EXISTS Developer(
   devID integer NOT NULL PRIMARY KEY,
+  studioID integer FOREIGN KEY REFERENCES Studio(studioID),
   name varchar(50),
-  yearsActive integer,
-  employer varchar(50) FOREIGN KEY REFERENCES Studio(name)
+  yearsActive integer
 );
 
-CREATE TABLE IF NOT EXISTS Franchise(
+CREATE TABLE IF NOT EXISTS Franchises(
   franchiseID integer NOT NULL PRIMARY KEY,
   name varchar(50)
 );
 
-CREATE TABLE IF NOT EXISTS WorldRecords(
+CREATE TABLE IF NOT EXISTS Stats(
+  statsID integer NOT NULL PRIMARY KEY,
+  gameID integer FOREIGN KEY REFERENCES Games(gameID),
+  avgTimeToBeat numeric
+);
+
+CREATE TABLE IF NOT EXISTS Records(
   recordID integer NOT NULL PRIMARY KEY,
-  game varchar(50) REFERNCES Game(title),
-  console varchar(50) FOREIGN KEY REFERENCES Console(name),
-  playerName varchar(50),
+  statsID integer FOREIGN KEY REFERENCES Stats(statsID),
+  title varchar(50),
+  player varchar(50),
   bestTime numeric
 );
 
-create table Console(
-  consoleID integer not null primary key,
-  name varchar(50),
-  sales numeric,
-  releaseYear integer,
-  type varchar(50)
+CREATE TABLE IF NOT EXISTS Platforms(
+  platformID integer NOT NULL PRIMARY KEY,
+  name varchar(50)
 );
 
-create table Game(
+CREATE TABLE IF NOT EXISTS Console(
+  consoleID integer not null primary key,
+  platformID integer FOREIGN KEY REFERENCES Platforms(platformID),
+  name varchar(50),
+  releaseYear integer,
+  sales numeric
+);
+
+CREATE TABLE IF NOT EXISTS PCClients(
+  pcClientsID integer not null primary key,
+  platformID integer FOREIGN KEY REFERENCES Platforms(platformID),
+  name varchar(50),
+  releaseYear integer
+)
+
+CREATE TABLE IF NOT EXISTS Games(
   gameID integer not null primary key,
   title varchar(50),
   console varchar(50) foreign key references Console(name),
